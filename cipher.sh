82a4e00423dc2e1dae6cc64d07c61d46e15c158f
AndresNeron <jorge.gonzalez@cimat.mx>
2026-08-19 13:36:03 -0600

    turing update

--- cipher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Encrypt credentials
-cd /home/ainode/Sync/lockerpy
-find $(pwd)/accounts -type f | grep -v bin >> enc_paths/accounts_path && sort -V -u -o enc_paths/accounts_path enc_paths/accounts_path
-sudo ./locker.py -rd /home/ainode/Sync/lockerpy/AES_keys/aes_key1.enc -rpem /home/ainode/Sync/lockerpy/RSA/lock.pem -ae -l enc_paths/accounts_path
