d136fb3cca65762ba5e6534f22416543ea8f5f9b
AndresNeron <jorge.gonzalez@cimat.mx>
2026-09-19 15:10:26 -0600

    turing update

---
+++ create_keys.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define paths
+PARENT_PATH="$(dirname "$(realpath "$0")")"
+cd "$PARENT_PATH"
+
+# Create directories where keys will live
+mkdir -p lockerpy/AES_keys/ lockerpy/src/lockerpy/RSA/
+
+# Create AES key
+locker -ag lockerpy/AES_keys/aes_key1
+
+# Create RSA key
+locker -rg lockerpy/src/lockerpy/RSA/lock
+
+# Cipher AES key with RSA public key
+locker -re AES_keys/aes_key1 -rpub src/lockerpy/RSA/lock_pem.pub
+
+# Setup the .env to start ciphering content
+cat << EOF 
+AES_KEY_PATH=$PARENT_PATH/lockerpy/AES_keys/aes_key1.enc
+RSA_KEY_PATH=$PARENT_PATH/lockerpy/src/lockerpy/RSA/lock.pem
+EOF
